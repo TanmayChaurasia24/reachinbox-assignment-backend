0df69ff749e8fc35c7404d820ef156480f010797
tanmay kumar chaurasia <tanmaychaurasia88@gmail.com>
2025-08-02 00:13:09 +0530

    search email by query working fine

--- src/controllers/emails/searchEmails.controller.ts
+++ src/controllers/emails/searchEmails.controller.ts
@@ -2,26 +2,30 @@ import type { Request, Response } from 'express';
 import { esClient } from '../../lib/elasticsearch.ts';
 
 export const searchEmails = async (req: Request, res: Response) => {
-  const queryText = req.query.query as string;
-
-  try {
-    const result = await esClient.search({
-      index: 'emails',
-      query: {
-        multi_match: {
-          query: queryText,
-          fields: ['subject', 'from', 'to'],
-        },
-      },
-    });
-
-    return res.status(200).json({
-      emails: result.hits.hits.map((hit) => hit._source),
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: 'Search failed',
-      error,
-    });
-  }
-};
+    const queryText = req.query.query as string;
+  
+    try {
+      const result = await esClient.search({
+        index: 'emails',
+        query: {
+          bool: {
+            should: [
+              { wildcard: { from: `*${queryText.toLowerCase()}*` } },
+              { wildcard: { to: `*${queryText.toLowerCase()}*` } },
+              { wildcard: { subject: `*${queryText.toLowerCase()}*` } }
+            ]
+          }
+        }
+      });
+  
+      return res.status(200).json({
+        emails: result.hits.hits.map((hit) => hit._source),
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: 'Search failed',
+        error,
+      });
+    }
+  };
+  
